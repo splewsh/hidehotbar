7663815e529bff155f0eba81f25af76d215691e7
Tom <stovonson@gmail.com>
2026-07-01 18:42:27 +0100

    Upgraded mod to 26.2

--- src/client/java/splewsh/hidehotbar/mixin/client/InGameHudMixin.java
+++ src/client/java/splewsh/hidehotbar/mixin/client/InGameHudMixin.java
@@ -1,25 +1,25 @@
 package splewsh.hidehotbar.mixin.client;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.CameraType;
-import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.DeltaTracker;
-import splewsh.hidehotbar.HideHotbarConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.Hud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import splewsh.hidehotbar.HideHotbarConfig;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class InGameHudMixin {
 
     @Inject(
-            method = "extractHotbarAndDecorations",
+            method = "extractItemHotbar",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void hideHotbarInThirdPerson(GuiGraphicsExtractor guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+    private void hideHotbarInThirdPerson(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
         CameraType camera = Minecraft.getInstance().options.getCameraType();
         HideHotbarConfig config = HideHotbarConfig.get();
 
